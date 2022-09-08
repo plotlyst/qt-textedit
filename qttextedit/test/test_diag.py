97dccc4e7dc658ba760e0bfc250b04ae46943ca6
Zsolt Kovari <zsolt@kovaridev.com>
2022-09-08 12:22:27 +0200

    Extract operations as actions (#25)
    * Add ops module
    
    * change op hierarchy
    
    * fix all ops
    
    * fix tests
    
    * update api

--- qttextedit/test/test_diag.py
+++ qttextedit/test/test_diag.py
@@ -1,5 +1,5 @@
-from qttextedit import LinkCreationDialog
-from .common import type_text
+from qttextedit.diag import LinkCreationDialog
+from qttextedit.test.common import type_text
 
 
 def test_link_creation_dialog_when_link_is_copied(qtbot):
--- qttextedit/util.py
+++ qttextedit/util.py
@@ -1,4 +1,7 @@
-from qtpy.QtGui import QTextCursor
+import qtawesome
+from qtpy.QtCore import QSize, Qt
+from qtpy.QtGui import QTextCursor, QIcon
+from qtpy.QtWidgets import QToolButton
 
 
 def select_anchor(cursor: QTextCursor) -> QTextCursor:
@@ -36,3 +39,25 @@ OPEN_QUOTATIONS = ('"', '"', '“', "«", "‹", "“")
 
 def is_open_quotation(char: str) -> bool:
     return char in OPEN_QUOTATIONS
+
+
+def button(icon: str, tooltip: str = '', shortcut=None, checkable: bool = True) -> QToolButton:
+    btn = QToolButton()
+    btn.setToolTip(tooltip)
+    btn.setIconSize(QSize(18, 18))
+    btn.setCursor(Qt.PointingHandCursor)
+    if icon.startswith('md') or icon.startswith('ri'):
+        btn.setIcon(qtawesome.icon(icon, options=[{'scale_factor': 1.2}]))
+    else:
+        btn.setIcon(qtawesome.icon(icon))
+    if shortcut:
+        btn.setShortcut(shortcut)
+    btn.setCheckable(checkable)
+
+    return btn
+
+
+def qta_icon(name: str) -> QIcon:
+    if name.startswith('md') or name.startswith('ri'):
+        return qtawesome.icon(name, options=[{'scale_factor': 1.2}])
+    return qtawesome.icon(name)
