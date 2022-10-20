d401b72a67817ca6aa8f0961c87eb5daf049c600
zkovari <zsolt@kovaridev.com>
2022-10-20 14:16:56 +0200

    toolbar ref

--- qttextedit/test/test_textedit.py
+++ qttextedit/test/test_textedit.py
@@ -91,41 +91,41 @@ def test_quotes(qtbot):
 def test_rich_texteditor(qtbot):
     editor = prepare_richtext_editor(qtbot)
 
-    assert editor._toolbar.standardOperation(TextEditorOperationType.BOLD).isVisible()
+    assert editor.toolbar().standardOperation(TextEditorOperationType.BOLD).isVisible()
 
 
 def test_bold_operation(qtbot):
     editor = prepare_richtext_editor(qtbot)
 
-    editor._toolbar.standardOperation(TextEditorOperationType.BOLD).trigger()
+    editor.toolbar().standardOperation(TextEditorOperationType.BOLD).trigger()
     assert editor.textEdit.fontWeight() == QFont.Bold
 
 
 def test_italic_operation(qtbot):
     editor = prepare_richtext_editor(qtbot)
 
-    editor._toolbar.standardOperation(TextEditorOperationType.ITALIC).trigger()
+    editor.toolbar().standardOperation(TextEditorOperationType.ITALIC).trigger()
     assert editor.textEdit.fontItalic()
 
 
 def test_underline_operation(qtbot):
     editor = prepare_richtext_editor(qtbot)
 
-    editor._toolbar.standardOperation(TextEditorOperationType.UNDERLINE).trigger()
+    editor.toolbar().standardOperation(TextEditorOperationType.UNDERLINE).trigger()
     assert editor.textEdit.fontUnderline()
 
 
 def test_strikethrough_operation(qtbot):
     editor = prepare_richtext_editor(qtbot)
 
-    editor._toolbar.standardOperation(TextEditorOperationType.STRIKETHROUGH).trigger()
+    editor.toolbar().standardOperation(TextEditorOperationType.STRIKETHROUGH).trigger()
     assert editor.textEdit.currentFont().strikeOut()
 
 
 def test_foreground_color(qtbot):
     editor = prepare_richtext_editor(qtbot)
 
-    color_op = editor._toolbar.standardOperation(TextEditorOperationType.COLOR)
+    color_op = editor.toolbar().standardOperation(TextEditorOperationType.COLOR)
     item = color_op.wdgTextStyle.wdgForeground.layout().itemAt(0)
     item.widget().click()
 
@@ -135,7 +135,7 @@ def test_foreground_color(qtbot):
 def test_background_color(qtbot):
     editor = prepare_richtext_editor(qtbot)
 
-    color_op = editor._toolbar.standardOperation(TextEditorOperationType.COLOR)
+    color_op = editor.toolbar().standardOperation(TextEditorOperationType.COLOR)
     item = color_op.wdgTextStyle.wdgBackground.layout().itemAt(0)
     item.widget().click()
 
