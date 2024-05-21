d53c1746cab9e2d4f9672a0b9bc5f97f47369f74
Zsolt Kovari <kovari.zsolt91@gmail.com>
2024-05-21 13:07:28 +0200

    Reformat frames to reapply padding and margins

--- qttextedit/api.py
+++ qttextedit/api.py
@@ -9,7 +9,7 @@ from qtpy import QtGui
 from qtpy.QtCore import Qt, QMimeData, QSize, QUrl, QBuffer, QIODevice, QPoint, QEvent, Signal, QMargins
 from qtpy.QtGui import QContextMenuEvent, QDesktopServices, QFont, QTextBlockFormat, QTextCursor, QTextList, \
     QTextCharFormat, QTextFormat, QTextBlock, QTextTable, QTextTableCell, QTextLength, QTextTableFormat, QKeyEvent, \
-    QColor, QWheelEvent
+    QColor, QWheelEvent, QTextFrame
 from qtpy.QtWidgets import QMenu, QWidget, QApplication, QFrame, QButtonGroup, QTextEdit, \
     QInputDialog, QToolButton, QLineEdit
 
@@ -510,6 +510,14 @@ class EnhancedTextEdit(QTextEdit):
         format.clearForeground()
         self.textCursor().setCharFormat(format)
 
+    def setText(self, text: str):
+        super().setText(text)
+        self._reformatFrames()
+
+    def setHtml(self, text: str):
+        super().setHtml(text)
+        self._reformatFrames()
+
     def setHeading(self, heading: int):
         cursor: QTextCursor = self.textCursor()
         cursor.beginEditBlock()
@@ -530,6 +538,18 @@ class EnhancedTextEdit(QTextEdit):
         cursor.clearSelection()
         self.setTextCursor(cursor)
         cursor.endEditBlock()
+
+    def _reformatFrames(self):
+        root: QTextFrame = self.document().rootFrame()
+        for frame in root.childFrames():
+            if isinstance(frame, QTextTable):
+                continue
+            frameFormat = frame.frameFormat()
+            frameFormat.setPadding(10)
+            frameFormat.setTopMargin(1)
+            frameFormat.setBottomMargin(1)
+            frameFormat.setBorder(1)
+            frame.setFrameFormat(frameFormat)
 
     def _adjustTabDistance(self):
         self.setTabStopDistance(QtGui.QFontMetricsF(self.font()).horizontalAdvance(' ') * 4)
