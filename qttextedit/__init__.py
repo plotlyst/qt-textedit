29c9c555ee8596916f0c05f378a7a0250ce33112
Zsolt Kovari <kovari.zsolt91@gmail.com>
2025-03-10 13:38:52 +0100

    Add EllipsisInsertionMode to init

--- qttextedit/__init__.py
+++ qttextedit/__init__.py
@@ -1,3 +1,4 @@
-from .api import RichTextEditor, EnhancedTextEdit, DashInsertionMode, AutoCapitalizationMode, TextBlockState, \
+from .api import RichTextEditor, EnhancedTextEdit, DashInsertionMode, AutoCapitalizationMode, EllipsisInsertionMode, \
+    TextBlockState, \
     TextEditorToolbar, StandardTextEditorToolbar, TextEditorSettingsButton
 from .util import remove_font, OBJECT_REPLACEMENT_CHARACTER
